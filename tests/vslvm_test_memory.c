07d92a16e101cb6fe35bdb6feb0eee961c551729
Joachim Metz <joachim.metz@gmail.com>
2019-10-19 08:47:47 -0700

    Applied updates

--- tests/vslvm_test_memory.c
+++ tests/vslvm_test_memory.c
@@ -35,15 +35,15 @@
 
 #if defined( HAVE_VSLVM_TEST_MEMORY )
 
-static void *(*vslvm_test_real_malloc)(size_t)                 = NULL;
-static void *(*vslvm_test_real_memcpy)(void *, void *, size_t) = NULL;
-static void *(*vslvm_test_real_memset)(void *, int, size_t)    = NULL;
-static void *(*vslvm_test_real_realloc)(void *, size_t)        = NULL;
+static void *(*vslvm_test_real_malloc)(size_t)                       = NULL;
+static void *(*vslvm_test_real_memcpy)(void *, const void *, size_t) = NULL;
+static void *(*vslvm_test_real_memset)(void *, int, size_t)          = NULL;
+static void *(*vslvm_test_real_realloc)(void *, size_t)              = NULL;
 
-int vslvm_test_malloc_attempts_before_fail                     = -1;
-int vslvm_test_memcpy_attempts_before_fail                     = -1;
-int vslvm_test_memset_attempts_before_fail                     = -1;
-int vslvm_test_realloc_attempts_before_fail                    = -1;
+int vslvm_test_malloc_attempts_before_fail                           = -1;
+int vslvm_test_memcpy_attempts_before_fail                           = -1;
+int vslvm_test_memset_attempts_before_fail                           = -1;
+int vslvm_test_realloc_attempts_before_fail                          = -1;
 
 /* Custom malloc for testing memory error cases
  * Note this function might fail if compiled with optimation
@@ -82,7 +82,7 @@ void *malloc(
  */
 void *memcpy(
        void *destination,
-       void *source,
+       const void *source,
        size_t size )
 {
 	if( vslvm_test_real_memcpy == NULL )
