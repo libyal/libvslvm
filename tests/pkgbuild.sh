dfd4a16d58cd34f39d45b3e2e22fd54c61a1a210
Joachim Metz <joachim.metz@gmail.com>
2026-09-16 12:37:08 +0200

    Worked on tests and changes for CI tests

--- tests/pkgbuild.sh
+++ tests/pkgbuild.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 # Script to build a MacOS pkg
 #
-# Version: 20241015
+# Version: 20260914
 
 set -e
 
-make install DESTDIR=${PWD}/osx-pkg
-mkdir -p ${PWD}/osx-pkg/usr/share/doc/libvslvm
-cp AUTHORS COPYING COPYING.LESSER NEWS README ${PWD}/osx-pkg/usr/share/doc/libvslvm
+make install DESTDIR="${PWD}/osx-pkg"
+mkdir -p "${PWD}/osx-pkg/usr/share/doc/libvslvm"
+cp AUTHORS COPYING COPYING.LESSER NEWS README "${PWD}/osx-pkg/usr/share/doc/libvslvm"
 
-VERSION=`sed '5!d; s/^  \[//;s/\],$//' configure.ac`
-pkgbuild --root osx-pkg --identifier com.github.libyal.libvslvm --version ${VERSION} --ownership recommended ../libvslvm-${VERSION}.pkg
+VERSION=$(sed '5!d; s/^  \[//;s/\],$//' configure.ac)
+pkgbuild --root osx-pkg --identifier com.github.libyal.libvslvm --version "${VERSION}" --ownership recommended "../libvslvm-${VERSION}.pkg"
 
--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libvslvm/.libs/libvslvm.1.dylib && test -f ./pyvslvm/.libs/pyvslvm.so
+if [ -f "${PWD}/libvslvm/.libs/libvslvm.1.dylib" ] && [ -f ./pyvslvm/.libs/pyvslvm.so ]
 then
-	install_name_tool -change /usr/local/lib/libvslvm.1.dylib ${PWD}/libvslvm/.libs/libvslvm.1.dylib ./pyvslvm/.libs/pyvslvm.so
+    install_name_tool -change /usr/local/lib/libvslvm.1.dylib "${PWD}/libvslvm/.libs/libvslvm.1.dylib" ./pyvslvm/.libs/pyvslvm.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
